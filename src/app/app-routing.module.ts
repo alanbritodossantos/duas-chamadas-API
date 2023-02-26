d3b987d1105d686dd62d35fbc9c9b0aa97ca84af
Alan <alanbritodossantos2015@gmail.com>
2023-02-26 03:06:23 -0300

    foi adicionado cor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { UserListComponent } from './pages/user-list/user-list.component';
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'user-details/:uuid', component: UserDetailsComponent },
+  {path:'',redirectTo:'/users',pathMatch:'full'},
 ];
 
 @NgModule({
